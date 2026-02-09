f08908476621f658fc1fe60e65bc4e2f53b4ab1e
Muralidhar8 <muralidhar8@users.noreply.github.com>
2026-02-09 11:37:53 +0530

    Configure GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Rail-madad-clone/',
   server: {
     proxy: {
-        '/api': 'http://localhost:5000'
+      '/api': 'http://localhost:5000'
     }
   }
 })
